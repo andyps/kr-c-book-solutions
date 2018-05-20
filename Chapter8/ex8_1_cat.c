ffd6a991ec1385c867dfd96fbaee74d93226ef5e
Andrey Pashentsev <avp200681@gmail.com>
2018-05-20 17:49:37 +0300

    8_1

--- Chapter8/ex8_1_cat.c
+++ Chapter8/ex8_1_cat.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fcntl.h"
-#include "unistd.h"
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 void filecopy(int ifp, int ofp) {
   char c;
